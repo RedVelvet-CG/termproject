3cebf5295cb558c7246b913f55bed633d2ee1157
Gummybearr <ehrudghl123@naver.com>
2020-05-18 16:44:18 +0900

    Move eye position z axis 100

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ uint				NUM_TESS = 50;		// initial tessellation factor of the circle as a polygo
 
 // common structures
 struct camera {
-	vec3	eye = vec3(0, 30, 300), at = vec3(0, 0, 0), up = vec3(0, 1, 0);
+	vec3	eye = vec3(0, 30, 400), at = vec3(0, 0, 0), up = vec3(0, 1, 0);
 	mat4	view_matrix = mat4::look_at(eye, at, up);
 	float	fovy = PI / 4.0f; // must be in radian
 	float	aspect = 0.f;
